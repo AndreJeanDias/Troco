30c1dee838e5c1894b31aa8bd7b6c2d77503078b
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 17:27:23 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("--- Cálculo de Troco ---\n");
+
+        Console.Write("Digite o valor da compra: R$ ");
+        double valorCompra = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor pago: R$ ");
+        double valorPago = double.Parse(Console.ReadLine());
+
+        double troco = valorPago - valorCompra;
+
+        Console.WriteLine("\nO valor do troco é: R$ " + troco.ToString("F2"));
+    }
+}
